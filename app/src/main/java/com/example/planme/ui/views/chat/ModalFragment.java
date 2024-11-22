780168f34e6280538be1125a806b7a04ace47e50
SaintCenteno2813 <erickcenteno0028@gmail.com>
2024-11-21 20:51:10 -0600

    Pantalla_QR

---
+++ app/src/main/java/com/example/planme/ui/views/chat/ModalFragment.java
@@ -0,0 +1,38 @@
+package com.example.planme.ui.views.chat;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.planme.R;
+
+public class ModalFragment extends Fragment {
+
+    private ModalViewModel mViewModel;
+
+    public static ModalFragment newInstance() {
+        return new ModalFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_modal, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ModalViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/planme/ui/views/chat/ModalViewModel.java
@@ -0,0 +1,7 @@
+package com.example.planme.ui.views.chat;
+
+import androidx.lifecycle.ViewModel;
+
+public class ModalViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
