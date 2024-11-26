67dab778069a3450d9ebf10310620562c18b79d1
Riqui503 <reggoer2000@gmail.com>
2024-11-26 09:20:09 -0600

    Merge remote-tracking branch 'refs/remotes/origin/devMarcos' into devReggie

--- app/src/main/java/com/example/planme/ui/views/me/MeFragment.java
+++ app/src/main/java/com/example/planme/ui/views/me/MeFragment.java
@@ -7,19 +7,26 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.planme.R;
 import com.example.planme.databinding.FragmentMeBinding;
 
 public class MeFragment extends Fragment {
 
+    NavController navController;
     FragmentMeBinding binding;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.navController = Navigation.findNavController(view);
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -28,38 +35,10 @@ public class MeFragment extends Fragment {
         MeViewModel viewModel = new ViewModelProvider(this).get(MeViewModel.class);
         this.binding = FragmentMeBinding.inflate(inflater, container, false);
 
-        Button btnInvitacion = binding.btnInvitacion;
-        Button btnAbout = binding.btnAbout;
-        Button btnCerrarSesion = binding.btnCerrarSesion;
-
-        btnInvitacion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "Invitación", Toast.LENGTH_SHORT).show();
-            }
+        this.binding.btnAbout.setOnClickListener(__ ->{
+            navController.navigate(R.id.navigation_me_to_navigation_About);
         });
 
-        btnAbout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "About", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "Cerrar sesión", Toast.LENGTH_SHORT).show();
-                // Aquí puedes añadir un Intent para cerrar sesión o volver al login, si es necesario
-            }
-        });
-
-        return this.binding.getRoot();
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        this.binding = null;
+        return null;
     }
 }
