1461ac660d105e5bc4becbf97b3e2965750907d7
marcosherna <marcosxdf6@gmail.com>
2024-12-05 13:09:38 -0600

    implementacion del cierre de session

--- app/src/main/java/com/example/planme/ui/views/me/MeFragment.java
+++ app/src/main/java/com/example/planme/ui/views/me/MeFragment.java
@@ -8,26 +8,34 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.example.planme.R;
 import com.example.planme.databinding.FragmentMeBinding;
+import com.example.planme.utils.ExceptionHelper;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MeFragment extends Fragment {
 
     NavController navController;
     FragmentMeBinding binding;
-    MeViewModel viewModel;
+    MeViewModel meViewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        viewModel = new ViewModelProvider(this).get(MeViewModel.class);
+        meViewModel = new ViewModelProvider(this).get(MeViewModel.class);
         this.binding = FragmentMeBinding.inflate(inflater, container, false);
 
         return this.binding.getRoot();
@@ -41,6 +49,46 @@ public class MeFragment extends Fragment {
         this.binding.btnAbout.setOnClickListener(__ ->{
             navController.navigate(R.id.navigation_me_to_navigation_About);
         });
+
+        if(meViewModel != null){
+            this.meViewModel.getUser().observe(getViewLifecycleOwner(), userUI -> {
+                if(userUI != null){
+                    this.binding.tvUser.setText(userUI.getName());
+                    this.binding.tvEmail.setText(userUI.getEmail());
+
+                    Glide.with(requireContext())
+                            .load(userUI.getUrlImg())
+                            .placeholder(R.drawable.ic_launcher_foreground)
+                            .error(R.drawable.ic_notifications_black_24dp)
+                            .transition(DrawableTransitionOptions.withCrossFade())
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .into(this.binding.imgUser);
+
+                }
+            });
+        }
+
+        if(binding != null){
+            this.binding.btnLogOut.setOnClickListener( __ -> this.handleLogOut());
+        }
+    }
+
+    private void handleLogOut(){
+        try {
+            this.meViewModel.signOut();
+            GoogleSignIn.getClient(requireContext(), GoogleSignInOptions.DEFAULT_SIGN_IN).signOut();
+
+            NavOptions navOptions = new NavOptions.Builder()
+                    .setPopUpTo(R.id.mobile_navigation, true)
+                    .build();
+
+            navController.navigate(R.id.navigation_login, null, navOptions);
+
+        } catch (Exception e){
+            Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
+            ExceptionHelper.log(e);
+        }
+
     }
 
 
--- app/src/main/java/com/example/planme/ui/views/me/MeViewModel.java
+++ app/src/main/java/com/example/planme/ui/views/me/MeViewModel.java
@@ -1,7 +1,45 @@
 package com.example.planme.ui.views.me;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.planme.ui.models.UserUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
 public class MeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser userSession;
+
+    MutableLiveData<UserUI> userUI = new MutableLiveData<>();
+
+    public MeViewModel(){
+        userSession = auth.getCurrentUser();
+        this.initialize();
+    }
+
+    public void initialize(){
+        if(this.userSession != null){
+
+            UserUI user = new UserUI(
+                    userSession.getUid(),
+                    userSession.getPhotoUrl(),
+                    userSession.getEmail(),
+                    userSession.getDisplayName());
+
+            this.userUI.setValue(user);
+        }
+    }
+
+    public void signOut(){
+        auth.signOut();
+    }
+
+    public LiveData<UserUI> getUser(){
+        return this.userUI;
+    }
+
 }
