f14f3605855de3f52eb9fbbf3e71763a735894fd
marcosherna <marcosxdf6@gmail.com>
2024-12-05 11:33:38 -0600

    merge to devReggi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -44,7 +45,18 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.legacy.support.v4)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
+//    implementation(libs.coordinatorlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
+    implementation("com.google.zxing:core:3.3.0")
 }
--- app/src/main/java/com/example/planme/ui/views/me/MeFragment.java
+++ app/src/main/java/com/example/planme/ui/views/me/MeFragment.java
@@ -21,24 +21,27 @@ public class MeFragment extends Fragment {
 
     NavController navController;
     FragmentMeBinding binding;
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        this.navController = Navigation.findNavController(view);
-    }
+    MeViewModel viewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        MeViewModel viewModel = new ViewModelProvider(this).get(MeViewModel.class);
+        viewModel = new ViewModelProvider(this).get(MeViewModel.class);
         this.binding = FragmentMeBinding.inflate(inflater, container, false);
+
+        return this.binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.navController = Navigation.findNavController(view);
 
         this.binding.btnAbout.setOnClickListener(__ ->{
             navController.navigate(R.id.navigation_me_to_navigation_About);
         });
-
-        return null;
     }
+
+
 }
