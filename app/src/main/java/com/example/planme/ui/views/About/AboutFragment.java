303a51e3b9cad1f48a2bdbe57cda0c35dd2a5eb6
Riqui503 <reggoer2000@gmail.com>
2024-12-06 12:32:55 -0600

    Merge remote-tracking branch 'refs/remotes/origin/devMarcos' into devReggie

--- app/src/main/java/com/example/planme/ui/views/About/AboutFragment.java
+++ app/src/main/java/com/example/planme/ui/views/About/AboutFragment.java
@@ -1,5 +1,6 @@
 package com.example.planme.ui.views.About;
 
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -7,6 +8,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +17,12 @@ import android.view.ViewGroup;
 
 import com.example.planme.R;
 import com.example.planme.databinding.FragmentAboutBinding;
+import com.example.planme.ui.views.Creadores.CreadoresFragment;
 
 public class AboutFragment extends Fragment {
 
     FragmentAboutBinding binding;
+    NavController controller;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -25,6 +30,17 @@ public class AboutFragment extends Fragment {
         this.binding = FragmentAboutBinding.inflate(inflater, container, false);
 
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.controller = Navigation.findNavController(view);
+        this.binding.creatorsButton.setOnClickListener(__->{
+            this.controller.navigate(R.id.action_navigation_About_to_navigation_Creadores);
+        });
+
     }
 }
 
---
+++ app/src/main/java/com/example/planme/ui/views/Creadores/CreadoresViewModel.java
@@ -0,0 +1,7 @@
+package com.example.planme.ui.views.Creadores;
+
+import androidx.lifecycle.ViewModel;
+
+public class CreadoresViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
